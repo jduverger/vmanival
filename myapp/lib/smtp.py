3f390cb01324fb1b0b56a4880b7f15eb677b4ed8
jackduvr <jduvr@orange.fr>
2023-04-07 10:07:04 +0200

    V1.4 mail params in .env

--- myapp/lib/smtp.py
+++ myapp/lib/smtp.py
@@ -2,18 +2,30 @@ import django
 from django.contrib.auth.models import User
 from django.conf import settings
 from myapp.lib import night
+import os
+import dotenv
 
 def set():
-    myemail='jduvr@orange.fr'
-    mysmtp='smtp.orange.fr'
-    user=User.objects.get(username=mysmtp)
+    #smtp_model='operator/smtpserv/port/username/pwd/ssl/tls'
+    dotenv_file = os.path.join(settings.BASE_DIR,".env")
+    if os.path.isfile(dotenv_file):
+        dotenv.load_dotenv(dotenv_file)
+    p = os.environ['smtp_params']
+    params=p.split('/')
+    mysmtp=params[1]
+    myemail=params[3]
+    #print("myemail ="+myemail)
     settings.EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     settings.EMAIL_HOST=mysmtp
-    settings.EMAIL_PORT=465
+    settings.EMAIL_PORT=params[2]
     settings.EMAIL_HOST_USER=myemail
-    settings.EMAIL_HOST_PASSWORD=night.dec(user.first_name)
-    settings.EMAIL_USE_SSL=True
-    settings.EMAIL_USE_TLS=False
+    settings.EMAIL_HOST_PASSWORD=night.dec(params[4])
+    ssl=params[5]
+    tls=params[6]
+    if ssl == "True":
+        settings.EMAIL_USE_SSL=True
+    if tls == "True":
+        settings.EMAIL_USE_TLS=True
     #settings.EMAIL_TIMEOUT=1
     settings.DEFAULT_FROM_EMAIL=myemail
     return 'OK'
