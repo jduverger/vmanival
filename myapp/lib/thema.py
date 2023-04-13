042c6e23bf8e5b6bcdbcbf7d39fe61674bbd9ea5
jackduvr <jduvr@orange.fr>
2023-04-13 14:28:40 +0200

    sort themes

--- myapp/lib/thema.py
+++ myapp/lib/thema.py
@@ -5,4 +5,6 @@ def tlist(thems):
             tthema[thema]+=1
         else:
             tthema[thema]=1
+    thema_sorted_by_value=sorted(tthema.items(), key=lambda x:x[1], reverse=True)
+    tthema=dict(thema_sorted_by_value)
     return tthema
